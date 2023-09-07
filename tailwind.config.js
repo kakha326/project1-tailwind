86314c8e12ab1435059dfffb128fce9ada8c81a7
kakha326 <k.mikeladze26@gmail.com>
2023-09-07 20:02:21 +0400

    sec6 update

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,13 +2,17 @@
 module.exports = {
   content: ['./*html'],
   theme: {
+   
     screens: {
     sm: '480px',
     md: '768px',
     lg: '976px',
     xl: '1440px',
-    },
+    } ,
     extend: {
+      // backgroundImage:{
+      //   'divImg' : "url('img/img 20.png')",
+      // },
   colors: {
     mainBgColor: '#F7F6F0',
   }
